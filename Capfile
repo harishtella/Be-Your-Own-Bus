8b1157c1de798aa1328bb8e71321c888b1e5ee0f
Harish Tella <ht@harishtella.info>
2009-12-05 11:28:09 -0600

    update capistrano configuration

--- Capfile
+++ Capfile
@@ -1,2 +1,18 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 load 'config/deploy'
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+   
+  task :restart, :roles => :app do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  task :after_symlink, :roles => :app do
+    run "rm -f ~/public_html;ln -s #{deploy_to}/current/public ~/public_html"
+    run "cp ~/environoment.rb #{deploy_to}/current/config/environoment.rb"
+  end
+end
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "byob"
 set :user, "harisht"
-set :repository,  "harisht@harishtella.info:/home/harisht/git/byob.git"
+set :domain, "harishtella.info"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -8,16 +8,16 @@ set :repository,  "harisht@harishtella.info:/home/harisht/git/byob.git"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-set :scm, :git
-set :scm_username, user
-set :runner, user
-set :use_sudo, false
-set :branch, "master"
-set :deploy_via, :checkout
-set :git_shallow_clone, 1
-set :deploy_to, "/home/#{user}/#{application}"
-default_run_options[:pty] = true
 
-#role :app, "your app-server here"
-#role :web, "your web-server here"
-#role :db,  "your db-server here", :primary => true
+set :repository,  "harisht@harishtella.info:/home/harisht/git/byob.git"
+set :scm, "git"
+
+
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_via, :checkout
+set :group_writable, false 
+
+role :app, domain 
+role :web, domain
+role :db,  domain, :primary => true
