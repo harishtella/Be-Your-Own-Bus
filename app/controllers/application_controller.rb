6121a887a1584df93e3514c98ba175f8afa8a834
Harish Tella <ht@harishtella.info>
2010-01-24 20:09:37 -0600

    turn off forgery protection to temporarily solve the ActionController::InvalidAuthenticityToken error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  #protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   ensure_application_is_installed_by_facebook_user
   attr_accessor :current_user
