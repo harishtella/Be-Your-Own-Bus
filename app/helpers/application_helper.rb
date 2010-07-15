87987e5b0cf493be9a6bb28a098e7144a36e82ab
Harish Tella <ht@harishtella.info>
2010-07-15 00:34:01 -0500

    get rid of syncenumerator usage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
-require 'generator'
 require 'date'
 
 module ApplicationHelper
   def gen_month_options(cur_month)
     month_names = Date::ABBR_MONTHNAMES[1,12]
     month_options = ""
+    month_numbers = (1..12).to_a.reverse
 
-    SyncEnumerator.new( (1..12), month_names).each do |option|
-      month_options += "<option value=\"#{option[0]}\"" 
-      month_options += if (option[0] == cur_month) then " selected " else "" end 
-      month_options += "> #{option[1]}</option>"
+    month_names.each do |mname|
+      mnum = month_numbers.pop
+      month_options += "<option value=\"#{mnum}\"" 
+      month_options += if (mnum == cur_month) then " selected " else "" end 
+      month_options += "> #{mname}</option>"
     end  
   end
 
