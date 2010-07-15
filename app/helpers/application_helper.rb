38aeedc0a5a9dd2b02039821e73c17a331127c0a
Harish Tella <ht@harishtella.info>
2010-07-15 00:11:18 -0500

    move some view code to helper to try to get rid of error with SyncEnumerator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'generator'
+require 'date'
+
 module ApplicationHelper
+  def gen_month_options(cur_month)
+    month_names = Date::ABBR_MONTHNAMES[1,12]
+    month_options = ""
+
+    SyncEnumerator.new( (1..12), month_names).each do |option|
+      month_options += "<option value=\"#{option[0]}\"" 
+      month_options += if (option[0] == cur_month) then " selected " else "" end 
+      month_options += "> #{option[1]}</option>"
+    end  
+  end
+
 end
