d8906bbce465ce62e31bba29a6ec989b165df6cb
Harish Tella <harishtella@gmail.com>
2011-06-17 14:05:55 -0500

    update facebooker.yml to use the new canvas url and same settings from production and testing
    make environment.rb specify rails version

--- db/schema.rb
+++ db/schema.rb
@@ -80,10 +80,10 @@ ActiveRecord::Schema.define(:version => 20100224231743) do
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "facebook_id",   :limit => 20,                    :null => false
+    t.integer  "facebook_id",   :default => -1,    :null => false
     t.string   "session_key"
     t.string   "email"
-    t.boolean  "email_revoked",               :default => false
+    t.boolean  "email_revoked", :default => false
     t.string   "name"
   end
 
